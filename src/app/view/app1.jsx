2956fb99f20db8c713f81ff9d9cda0adf693b360
Gerard Muñoz <gmunoz1979@gmail.com>
2017-01-11 15:33:22 -0300

    Se anaden nuevos elementos al formulario.

--- src/app/view/app1.jsx
+++ src/app/view/app1.jsx
@@ -1,7 +1,11 @@
-import React    from "react";
-import Form     from "../../lib/ui/form/form";
-import Row      from "../../lib/ui/form/row";
-import Combobox from "../../lib/ui/form/combobox";
+import React         from "react";
+import Panel         from "../../lib/ui/panel";
+import Form          from "../../lib/ui/form/form";
+import Row           from "../../lib/ui/form/row";
+import Combobox      from "../../lib/ui/form/combobox";
+import TextField     from "../../lib/ui/form/textfield";
+import TextAreaField from "../../lib/ui/form/textareafield";
+import CheckboxField from "../../lib/ui/form/checkboxfield";
 
 class App1 extends React.Component {
 
@@ -9,30 +13,65 @@ class App1 extends React.Component {
     return (
       <div className="app1">
         <span className="title">Ejemplo 1</span>
-        <div className="wrap">
+        <Panel
+          width  = "500"
+          height = "250"
+          >
           <Form>
             <Row>
+              <TextField
+                title      = "Id"
+                titleWidth = "80"
+                name       = "id"
+                width      = "250"
+              />
+            </Row>
+            <Row>
               <Combobox
-                title      = "1 Combobox"
-                titleWidth = "60"
-                name       = "cbx1"
+                title      = "Tipo"
+                titleWidth = "80"
+                name       = "tipo"
                 width      = "auto"
-                namespace  = "Folder"
+                namespace  = "Nivel1/Tipo"
                 idValue    = "fold_id"
                 textValue  = "name"
-                />
+              />
               <Combobox
-                title      = "2 Combobox"
-                titleWidth = "60"
-                name       = "cbx2"
+                title      = "Sub tipo"
+                titleWidth = "80"
+                name       = "subtipo"
                 width      = "200"
-                namespace  = "Folder"
+                namespace  = "Nivel1/SubTipo"
                 idValue    = "fold_id"
                 textValue  = "name"
-                />
+              />
+            </Row>
+            <Row>
+              <TextField
+                title      = "Nombre"
+                titleWidth = "80"
+                name       = "nombre"
+                width      = "auto"
+              />
+            </Row>
+            <Row>
+              <TextAreaField
+                title      = "Descripcion"
+                titleWidth = "80"
+                name       = "descripcion"
+                width      = "auto"
+                height     = "100"
+              />
+            </Row>
+            <Row>
+              <CheckboxField
+                text    = "A Bool"
+                checked = "false"
+                widht   = "100"
+              />
             </Row>
           </Form>
-        </div>
+        </Panel>
       </div>
     );
   }
