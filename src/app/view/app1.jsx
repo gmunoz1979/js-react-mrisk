26a164ffdd009698ed37425439a7157124020643
Gerard Muñoz <gmunoz1979@gmail.com>
2017-01-12 17:11:23 -0300

    Se actualiza la App.

--- src/app/view/app1.jsx
+++ src/app/view/app1.jsx
@@ -12,69 +12,104 @@ import Column        from "../../lib/ui/column";
 class App1 extends React.Component {
 
   render() {
+    let form = (
+      <Form
+        namespace = "Nivel1"
+        fieldKey  = "niv1_id"
+        >
+        <Row>
+          <TextField
+            title      = "Id"
+            titleWidth = "80"
+            name       = "niv1_id"
+            width      = "250"
+          />
+        </Row>
+        <Row>
+          <Combobox
+            title      = "Tipo"
+            titleWidth = "80"
+            name       = "tipo"
+            width      = "auto"
+            namespace  = "Nivel1/Tipo"
+            idValue    = "tipo_id"
+            textValue  = "name"
+          />
+          <Combobox
+            title      = "Sub tipo"
+            titleWidth = "80"
+            name       = "subtipo"
+            width      = "200"
+            namespace  = "Nivel1/SubTipo"
+            idValue    = "subt_id"
+            textValue  = "name"
+            filterBy   = "tipo"
+          />
+        </Row>
+        <Row>
+          <TextField
+            title      = "Nombre"
+            titleWidth = "80"
+            name       = "name"
+            width      = "auto"
+          />
+        </Row>
+        <Row>
+          <TextAreaField
+            title      = "Descripcion"
+            titleWidth = "80"
+            name       = "description"
+            width      = "auto"
+            height     = "100"
+          />
+        </Row>
+        <Row>
+          <CheckboxField
+            name    = "a_bool"
+            text    = "A Bool"
+            checked = "false"
+            widht   = "100"
+          />
+        </Row>
+      </Form>
+    );
+
+    let table1 = (
+      <Table
+        name      = "table1"
+        namespace = "Nivel2"
+        filterBy  = "niv1_id"
+        idKey     = "niv2_id"
+      >
+        <Column name = "niv2_id"   title = "Id"           width = "50"></Column>
+        <Column name = "name"      title = "Nombre"       width = "auto"></Column>
+        <Column name = "ok"        title = "OK"           width = "50"></Column>
+        <Column name = "last_date" title = "Ultima Fecha" width = "auto"></Column>
+        <Column name = "other"     title = "Otro"         width = "auto"></Column>
+      </Table>
+    );
+
+    let table2 = (
+      <Table
+        name         = "table2"
+        namespace    = "Nivel3"
+        objectParent = "table1"
+        idKey        = "niv3_id"
+      >
+        <Column name = "niv3_id"     title = "Id"          width = "50"></Column>
+        <Column name = "tipo.name"   title = "Tipo"        width = "100"></Column>
+        <Column name = "description" title = "Descripcion" width = "auto"></Column>
+      </Table>
+    );
+
     return (
-      <div className="app">
+      <div className="app container">
         <div className="app1">
           <span className="title">Ejemplo 1</span>
           <Panel
             width  = "500"
-            height = "250"
-            >
-            <Form>
-              <Row>
-                <TextField
-                  title      = "Id"
-                  titleWidth = "80"
-                  name       = "id"
-                  width      = "250"
-                />
-              </Row>
-              <Row>
-                <Combobox
-                  title      = "Tipo"
-                  titleWidth = "80"
-                  name       = "tipo"
-                  width      = "auto"
-                  namespace  = "Nivel1/Tipo"
-                  idValue    = "tipo_id"
-                  textValue  = "name"
-                />
-                <Combobox
-                  title      = "Sub tipo"
-                  titleWidth = "80"
-                  name       = "subtipo"
-                  width      = "200"
-                  namespace  = "Nivel1/SubTipo"
-                  idValue    = "subt_id"
-                  textValue  = "name"
-                  filterBy   = "tipo"
-                />
-              </Row>
-              <Row>
-                <TextField
-                  title      = "Nombre"
-                  titleWidth = "80"
-                  name       = "nombre"
-                  width      = "auto"
-                />
-              </Row>
-              <Row>
-                <TextAreaField
-                  title      = "Descripcion"
-                  titleWidth = "80"
-                  name       = "descripcion"
-                  width      = "auto"
-                  height     = "100"
-                />
-              </Row>
-              <Row>
-                <CheckboxField
-                  text    = "A Bool"
-                  checked = "false"
-                  widht   = "100"
-                />
-              </Row>
-            </Form>
+            height = "250">
+            {form}
           </Panel>
         </div>
         <div className="app2">
@@ -82,13 +117,7 @@ class App1 extends React.Component {
           <Panel
             width  = "500"
             height = "270">
-            <Table namespace = "Nivel2">
-              <Column name="niv2_id"   title="Id"           width="50"></Column>
-              <Column name="name"      title="Nombre"       width="auto"></Column>
-              <Column name="ok"        title="OK"           width="50"></Column>
-              <Column name="last_date" title="Ultima Fecha" width="auto"></Column>
-              <Column name="other"     title="Otro"         width="auto"></Column>
-            </Table>
+            {table1}
           </Panel>
         </div>
         <div className="app3">
@@ -96,11 +125,7 @@ class App1 extends React.Component {
           <Panel
             width  = "500"
             height = "430">
-            <Table namespace = "Nivel3">
-              <Column name="niv3_id"     title="Id"          width="50"></Column>
-              <Column name="tipo.name"   title="Tipo"        width="100"></Column>
-              <Column name="description" title="Descripcion" width="auto"></Column>
-            </Table>
+            {table2}
           </Panel>
         </div>
       </div>
