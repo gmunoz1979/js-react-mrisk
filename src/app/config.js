6eda6975b7c3f1b1e30d898e06865a98f55138d5
Gerard Muñoz <gmunoz1979@gmail.com>
2017-01-16 19:52:11 -0300

    Fix redirect

--- src/app/config.js
+++ src/app/config.js
@@ -4,8 +4,8 @@ Config.Url     = "https://local.halftau.com"
 Config.Context = "Pruebas";
 
 const client_id = 540289891877889;
-const url       = document.location.origin;
+const url       = document.location.href;
 
-Config.Redirect = `https://local.halftau.com/Login/Site/form?response_type=code&client_id=${client_id}&redirect_uri=${url}/redirect.html&scope=Login&login_hint=&state=`;
+Config.Redirect = `https://local.halftau.com/Login/Site/form?response_type=code&client_id=${client_id}&redirect_uri=${url}redirect.html&scope=Login&login_hint=&state=`;
 
 export default Config;
