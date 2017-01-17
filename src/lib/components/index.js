d0f03413fda96db334af032ade7e85e27da0dbf2
Gerard Muñoz <gmunoz1979@gmail.com>
2017-01-17 12:10:06 -0300

    Se anaden los index.js por carpeta.

---
+++ src/lib/components/index.js
@@ -0,0 +1,6 @@
+export { default as Form }    from "./form";
+export { default as Alert }   from "./alert";
+export { default as Column }  from "./column";
+export { default as Message } from "./message";
+export { default as Panel }   from "./panel";
+export { default as Table }   from "./table";
