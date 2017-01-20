9d10ac94d2a4a18b189d1d0c978bcad4e4244a35
Gerard Muñoz <gmunoz1979@gmail.com>
2017-01-20 13:34:51 -0300

    Field. Se cambia forma de asignar el objeto component,

--- src/lib/components/form/field.jsx
+++ src/lib/components/form/field.jsx
@@ -2,8 +2,14 @@ import React  from "react";
 
 class Field extends React.Component {
 
-  componentDidMount() {
-    this.field.component = this;
+  set field(value) {
+    value = value || {};
+    value.component = this;
+    this._field = value;
+  }
+
+  get field() {
+    return this._field;
   }
 
   get value() {
