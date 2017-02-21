d3f3cffd351238394eb80a07f770e69e1231abbd
Gerard Muñoz <gmunoz1979@gmail.com>
2017-02-21 00:06:55 -0300

    Add form export Lib.

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,6 +1,8 @@
-//export { default as Form }    from "./form";
+import * as Form from "./form";
+
 export { default as Column }  from "./column";
 export { default as Message } from "./message";
 export { default as Panel }   from "./panel";
 export { default as Table }   from "./table";
 export { default as Router }  from "./router";
+export { Form }
