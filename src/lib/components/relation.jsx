62dcf7a860e497ca1462b3c044dcc4e983570b2f
Gerard Muñoz <gmunoz1979@gmail.com>
2017-02-26 23:21:25 -0300

    Relation. Se renombra variable.

--- src/lib/components/relation.jsx
+++ src/lib/components/relation.jsx
@@ -7,24 +7,24 @@ import Util          from "../util";
 class Relation extends React.Component {
 
   componentDidMount() {
-    const relTo   = Util.findReact(this.target.closest(".combobox")) ||
+    const relFrom = Util.findReact(this.target.closest(".combobox")) ||
                     Util.findReact(this.target.closest("form"));
     const relWith = Util.findReact(document.body.querySelector(`*[name=${this.props.with}]`));
 
     if (relWith instanceof IdSearchForm) {
       relWith.form.addEventListener("new", e => {
-        relTo instanceof FetchableForm &&
-          relTo.clear();
+        relFrom instanceof FetchableForm &&
+          relFrom.clear();
       });
 
       relWith.form.addEventListener("edit", e => {
-        relTo instanceof FetchableForm &&
-          relTo.fetchById([e.detail.id]);
+        relFrom instanceof FetchableForm &&
+          relFrom.fetchById([e.detail.id]);
       });
 
       relWith.form.addEventListener("view", e => {
-        relTo instanceof FetchableForm &&
-          relTo.fetchById([e.detail.id]);
+        relFrom instanceof FetchableForm &&
+          relFrom.fetchById([e.detail.id]);
       });
 
       return;
@@ -34,8 +34,8 @@ class Relation extends React.Component {
       const target = relWith._field;
       target.addEventListener("change", e =>
         {
-          if (relTo instanceof Combobox) {
-            let namespace = relTo._field.parentNode.querySelector(".namespace");
+          if (relFrom instanceof Combobox) {
+            let namespace = relFrom._field.parentNode.querySelector(".namespace");
             namespace = Util.findReact(namespace);
             namespace.getData({ id: [relWith.value] });
             return;
