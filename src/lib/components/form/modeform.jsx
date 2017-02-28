80e89d06c374f69c99109496e6d21b3f487609a1
Gerard Muñoz <gmunoz1979@gmail.com>
2017-02-28 15:45:58 -0300

    ModeForm. Se anade logica ModeForm.

--- src/lib/components/form/modeform.jsx
+++ src/lib/components/form/modeform.jsx
@@ -1,8 +1,13 @@
-import React  from "react";
+import React     from "react";
+import KeyMirror from "keymirror";
+import Field     from "./field";
+import Combobox  from "./combobox";
+import Textfield from "./textfield";
+
+const MODE = KeyMirror({ NEW: null, EDIT: null, VIEW: null });
 
 class ModeForm extends React.Component {
-
-  static mode = { NEW: 0, EDIT: 1, VIEW: 2 }
+  static mode = MODE
 
   static defaultProps = {
     mode: ModeForm.mode.VIEW
@@ -14,9 +19,56 @@ class ModeForm extends React.Component {
     this.setState({ mode: this.props.mode });
   }
 
+  changeMode(mode) {
+    this.setState({ mode: mode });
+  }
+
+  isType(c, o) {
+    if (c === o) {
+      return true;
+    }
+    if (c === React.Component) {
+      return false;
+    }
+
+    return this.isType(c.__proto__, o);
+  }
+
+  overrideFields(children) {
+    return React.Children.map(children, c =>
+      {
+        let props    = c.props;
+        let children = c.props.children;
+
+        if (this.isType(c.type, Field)) {
+          props = Object.assign({}, props,
+            {
+              readOnly: this.state.mode === MODE.VIEW
+            });
+
+          if (this.isType(c.type, Combobox) && this.state.mode === MODE.VIEW) {
+            props.name = `${props.object}.${props.textValue}`;
+            delete props.object;
+            delete props.idValue;
+            delete props.textValue;
+            c = <Textfield />;
+          }
+        }
+
+        if (children) {
+          return React.cloneElement(c, props, this.overrideFields(children));
+        } else {
+          return React.cloneElement(c, props);
+        }
+      }
+    );
+  }
+
   render() {
+    const children = this.overrideFields(this.props.children);
+
     return (
-      <div className="modeform"></div>
+      <div className="modeform">{children}</div>
     );
   }
 }
