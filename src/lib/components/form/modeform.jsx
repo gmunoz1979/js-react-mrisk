d5fc366a26a3883a227697a002cc87c7d3e15882
Gerard Muñoz <gmunoz1979@gmail.com>
2017-03-01 11:51:19 -0300

    Combobox. Relación en modo VIEW.

--- src/lib/components/form/modeform.jsx
+++ src/lib/components/form/modeform.jsx
@@ -3,6 +3,7 @@ import KeyMirror from "keymirror";
 import Field     from "./field";
 import Combobox  from "./combobox";
 import Textfield from "./textfield";
+import Relation  from "../relation";
 
 const MODE = KeyMirror({ NEW: null, EDIT: null, VIEW: null });
 
@@ -34,7 +35,50 @@ class ModeForm extends React.Component {
     return this.isType(c.__proto__, o);
   }
 
+  getAllRelations(children, parent, relations={}) {
+
+    React.Children.forEach(children, c =>
+      {
+        const children = c.props.children;
+
+        if (parent && this.isType(c.type, Relation) && this.isType(parent.type, Combobox)) {
+          const n = parent.props.name;
+          const w = c.props.with;
+          let rn = relations[n] = relations[n] || { root: "" };
+          let rw = relations[w] = relations[w] || { root: "" };
+
+          rn.child  = w;
+          rw.parent = n;
+          rw.root   = parent.props.object + ".";
+        }
+
+        if (children) {
+          return this.getAllRelations(children, c, relations);
+        }
+      }
+    );
+
+    return relations;
+  }
+
+  walkRelation(relations, relation, root="") {
+    if (!relation) return;
+
+    root += relation.root;
+    this.walkRelation(relations, relations[relation.child], root);
+    relation.root = root;
+  }
+
+  processRelations(relations) {
+    for (let [k, v] of Object.entries(relations)) {
+      !v.parent && this.walkRelation(relations, v);
+    }
+  }
+
   overrideFields(children) {
+    const relations = this.getAllRelations(children);
+    this.processRelations(relations);
+
     return React.Children.map(children, c =>
       {
         let props    = c.props;
@@ -47,7 +91,8 @@ class ModeForm extends React.Component {
             });
 
           if (this.isType(c.type, Combobox) && this.state.mode === MODE.VIEW) {
-            props.name = `${props.object}.${props.textValue}`;
+            props.name = `${relations[props.name].root}${props.object}.${props.textValue}`;
+
             delete props.object;
             delete props.idValue;
             delete props.textValue;
